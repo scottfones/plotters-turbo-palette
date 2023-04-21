b272ff2e38e241d55dd4c9125c5c39b4342b4c1a
Scott Fones <scott.fones@gmail.com>
2023-04-21 14:28:12 -0400

    added enum for palette names. added filename construction function.

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,52 @@ use plotters::prelude::*;
 
 mod custom_palettes;
 
+enum CustomPalette {
+    BentCoolWarm,
+    BlackBody,
+    ExtendedKindlmann,
+    Inferno,
+    Kindlmann,
+    Plasma,
+    SmoothCoolWarm,
+    Turbo,
+    Viridis,
+}
+use CustomPalette::*;
+
+fn set_filename(p_name: &CustomPalette) -> String {
+    let sample_name = match p_name {
+        BentCoolWarm => "bent-cool-warm",
+        BlackBody => "black-body",
+        ExtendedKindlmann => "extended-kindlmann",
+        Inferno => "inferno",
+        Kindlmann => "kindlmann",
+        Plasma => "plasma",
+        SmoothCoolWarm => "smooth-cool-warm",
+        Turbo => "turbo",
+        Viridis => "viridis",
+    };
+    format!("heatmap-{}.png", sample_name)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     const HEIGHT: u32 = 100;
     const WIDTH: f64 = 800.0;
 
     let palettes = vec![
-        "bent-cool-warm",
-        "black-body",
-        "extended-kindlmann",
-        "inferno",
-        "kindlmann",
-        "plasma",
-        "smooth-cool-warm",
-        "turbo",
-        "viridis",
+        BentCoolWarm,
+        BlackBody,
+        ExtendedKindlmann,
+        Inferno,
+        Kindlmann,
+        Plasma,
+        SmoothCoolWarm,
+        Turbo,
+        Viridis,
     ];
 
     for name in palettes {
-        let filename = format!("heatmap-{}.png", &name);
+        let filename = set_filename(&name);
         let root_drawing_area =
             BitMapBackend::new(&filename, (WIDTH as u32, HEIGHT)).into_drawing_area();
         let child_drawing_areas = root_drawing_area.split_evenly((1, WIDTH as usize));
@@ -27,35 +55,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         for (idx, area) in child_drawing_areas.iter().enumerate() {
             let color_code = ((idx as f64 / WIDTH) * 1024.0) as usize;
             match name {
-                "bent-cool-warm" => {
+                BentCoolWarm => {
                     area.fill(&custom_palettes::PaletteBentCoolWarm1024::pick(color_code))?
                 }
 
-                "black-body" => {
-                    area.fill(&custom_palettes::PaletteBlackBody1024::pick(color_code))?
-                }
+                BlackBody => area.fill(&custom_palettes::PaletteBlackBody1024::pick(color_code))?,
 
-                "extended-kindlmann" => area.fill(
+                ExtendedKindlmann => area.fill(
                     &custom_palettes::PaletteExtendedKindlmann1024::pick(color_code),
                 )?,
 
-                "inferno" => area.fill(&custom_palettes::PaletteInferno1024::pick(color_code))?,
+                Inferno => area.fill(&custom_palettes::PaletteInferno1024::pick(color_code))?,
 
-                "kindlmann" => {
-                    area.fill(&custom_palettes::PaletteKindlmann1024::pick(color_code))?
-                }
+                Kindlmann => area.fill(&custom_palettes::PaletteKindlmann1024::pick(color_code))?,
 
-                "plasma" => area.fill(&custom_palettes::PalettePlasma1024::pick(color_code))?,
+                Plasma => area.fill(&custom_palettes::PalettePlasma1024::pick(color_code))?,
 
-                "smooth-cool-warm" => area.fill(
-                    &custom_palettes::PaletteSmoothCoolWarm1024::pick(color_code),
-                )?,
+                SmoothCoolWarm => area.fill(&custom_palettes::PaletteSmoothCoolWarm1024::pick(
+                    color_code,
+                ))?,
 
-                "turbo" => area.fill(&custom_palettes::PaletteTurbo256::pick(color_code / 4))?,
+                Turbo => area.fill(&custom_palettes::PaletteTurbo256::pick(color_code / 4))?,
 
-                "viridis" => area.fill(&custom_palettes::PaletteViridis1024::pick(color_code))?,
-
-                _ => unreachable!("unknown palette name: {name}"),
+                Viridis => area.fill(&custom_palettes::PaletteViridis1024::pick(color_code))?,
             };
         }
         root_drawing_area.present()?;
